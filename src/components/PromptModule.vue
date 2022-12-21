42cd99b69c11bf05e0228f6d2b69a0eaf5411ed7
jamesahnking <jamesahnking@users.noreply.github.com>
2022-12-21 18:51:46 -0500

    Tweaked layout

--- src/components/PromptModule.vue
+++ src/components/PromptModule.vue
@@ -1,9 +1,17 @@
 <template>
-    <div class="grid grid-nogutter surface-card">
+    <div class="grid grid-nogutter surface-card col-12">
+
+        <Dialog v-model:visible="displayBasic" :breakpoints="{ '960px': '75vw', '640px': '90vw' }"
+            :style="{ width: '50vw' }">
+            <p>Make sure your add text to the prompt killa!</p>
+            <template #footer>
+                <Button label="Got It!" icon="pi pi-check" @click="closeBasic" autofocus />
+            </template>
+        </Dialog>
 
         <!-- COl1 -->
         <div class="col-12 md:col-12 lg:col-6">
-            <div class="p-7 ">
+            <div class="p-4 ">
                 <div class="border-round surface-card">
                     <h1>OpenAI Image Generator</h1>
                     <p>Describe a fantastical image in words, and our magical AI will created its best representation on
@@ -12,7 +20,7 @@
                 <label for="email4" class="block text-900 font-xs mb-2">Image Description</label>
                 <InputText id="email4" v-model="prompt" type="text" class="w-full mb-3 p-3"
                     placeholder="Ex. A dragon sipping a beer on a volcano" />
-                <Button @click="sendText" label="Generate a New AI Image" class="w-full py-3 "></Button>
+                <Button @click="sendText" label="Generate a new AI Image" class="pi-pallet w-full py-3"></Button>
             </div>
         </div>
 
@@ -20,20 +28,15 @@
         <div class="col-12 md:col-12 lg:col-6">
             <div class="p-0">
                 <div class="border-round surface-card">
-
-                    <div v-if=!resImage>
-                        <img :src="srcImage" alt="img" class="image-fit" />                    
+                    <div v-show="loading">
+                        <ProgressBar mode="indeterminate" style="height: .5em" />
                     </div>
-
-                    <div v-if=loading>
-                                
-                        <ProgressBar mode="indeterminate" style="height: .20em" />
+                    <div v-if="resImage.length">
+                        <img :src="`${resImage}`" class="image-fit">
                     </div>
-
                     <div v-else>
-                        <img :src="resImage" alt="img" class="image-fit" />
+                        <img :src="`${srcImage}`" class="image-fit">
                     </div>
-
                 </div>
             </div>
         </div>
@@ -54,11 +57,11 @@ export default defineComponent({
         return {
             prompt: '',
             resImage: '',
-            srcImage: srcImage,
+            srcImage,
             imageHeight: '500px',
-            imageVal: srcImage,
             loading: false,
-
+            error: false,
+            displayBasic: false,
         }
     },
     mounted() {
@@ -66,15 +69,35 @@ export default defineComponent({
     methods: {
         // Send text to AI  
         async sendText() {
-            this.loading = true;
-            const res = await axios.post(url, {
-                prompt: this.prompt,
-            })
-            this.resImage = res.data.data;
-            console.log(`Hey its RESIMAGE ${this.resImage}`)
-            this.loading = false;
+            try {
+                this.loading = true;
+                const res = await axios.post(url, {
+                    prompt: this.prompt,
+                })
 
-        }
+                this.resImage = res.data.data;
+
+                if (res.data.length) {
+                    this.loading = false;
+                }
+                console.log(`Hey its RESIMAGE ${this.resImage} this.loading = ${this.loading}`)
+
+            } catch (err) {
+
+                this.loading = false;
+                this.error = true;
+                this.openBasic();
+                console.log(`Error - YOu need to add text for this to work`)
+            }
+        },
+        openBasic() {
+            this.displayBasic = true;
+        },
+        closeBasic() {
+            this.displayBasic = false;
+        },
+
+
     },
     components: {},
 });
